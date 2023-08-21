08cb90f8606c10d6a096821cf34caa8b07841283
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-21 03:58:06 -0400

    fix

--- _printf.c
+++ _printf.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
-* _printf - a custom functioon that acts like prinf
-* @format: the string format that is used  for printing
+* _printf - a custom function that acts like printf
+* @format: the string format that is used for printing
 *
 * Return: The number of characters printed
 */
 int _printf(const char *format, ...)
 {
-int printed_chars = 0, i, (*identifier)(va_list);
+int printed_chars = 0, i, (*identifier)(va_list), j;
 char identifiers[3];
 va_list args;
 if (format == NULL)
-{
 return (-1);
-}
 identifiers[2] = '\0';
 va_start(args, format);
 for (i = 0; format[i]; i++)
@@ -22,18 +20,21 @@ for (i = 0; format[i]; i++)
 if (format[i] == '%')
 {
 identifiers[0] = '%';
-identifiers[1] = format[i + 1];
+j = i + 1;
+while (format[j] == ' ')
+j++;
+identifiers[1] = format[j];
 identifier = identifierFunc(identifiers);
 if (identifier)
 {
 printed_chars += identifier(args);
-i++;
+i = j;
 }
-else if (format[i + 1] != '\0')
+else if (format[j] != '\0')
 {
 printed_chars += printer_fun('%');
-printed_chars += printer_fun(format[i + 1]);
-i++;
+printed_chars += printer_fun(format[j]);
+i = j;
 }
 else
 {
